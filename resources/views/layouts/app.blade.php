e8132fada9071560b4641361afe64e0efda78fee
Muhammad Surya Saniansyah <muhammadsurya216@gmail.com>
2026-09-05 15:40:16 +0800

    fix: update title separator in app layout from "&mdash;" to "-"

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png') }}">
     <title>
-        @yield('title') &mdash; SIMPEL
+        @yield('title') - SIMPEL
     </title>
 
     <!-- Font Awesome Icons -->
